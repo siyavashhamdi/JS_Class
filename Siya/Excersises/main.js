d4a147fdd9660b28c7aed9e1a7d02449bab1c6f3
siyavashhamdi <siyavashhamdi@github.com>
2018-06-17 00:37:56 +0430

    A little change

--- Siya/Excersises/main.js
+++ Siya/Excersises/main.js
@@ -5,19 +5,4 @@
     elemMyDiv.innerHTML += "<BR />";
 }
 
-
-
-// Array
-
-print('spread array:')
-
-const arr1 = [1, 2, 3]
-const arr2 = ['x', 'y', 'z']
-const obj1 = { a: 1, b: 2, c: 3 };
-
-print(JSON.stringify(['a', ...arr1, 'b', 'c', ...arr2]))
-
-// cloning
-print('spread to clone:')
-print(JSON.stringify([...arr1]))
-print(JSON.stringify({ ...obj1 }))
+print('Test')
